d6c3f96023eed51b289b29cc9c6626f35ef505a8
khuzaima yaseen <khuzaimayaseen@gmail.com>
2020-11-01 15:36:56 +0500

    Some optimizations more more

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,45 @@
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "../theme";
+import { useRouter } from "next/router";
 
+import Spinner from "../Components/Spinner/Spinner";
 import Layout from "../Components/Layout/Layout";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setLoading(true);
+    };
+    const handleRouteComplete = () => {
+      setLoading(false);
+    };
+    router.events.on("routeChangeStart", handleRouteChange);
+    router.events.on("routeChangeComplete", handleRouteComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", () => handleRouteChange);
+      router.events.off("routeChangeStart", () => handleRouteComplete);
+    };
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
     </ThemeProvider>
   );
 }

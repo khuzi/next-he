30cfd49fd45637d296d0ccb7e835a984a276121f
khuzaima yaseen <khuzaimayaseen@gmail.com>
2020-11-02 12:06:06 +0500

    Change path and priority in Image 1

--- src/Components/GsllerySlider/GallerySlider.js
+++ src/Components/GsllerySlider/GallerySlider.js
@@ -17,19 +17,19 @@ const GallerySlider = () => {
         <div className={classes.overlay}>
           <Grid container alignItems="center" spacing={2}>
             <Grid item xs={12} md={6} lg={4}>
-              <Image src="/images/1.jpg" unsized loading="eager" priority/>
+              <Image src="/images/1.jpg" unsized priority />
             </Grid>
             <Grid item xs={12} md={6} lg={4}>
-              <Image src="/images/2.jpg" unsized priority/>
+              <Image src="/images/2.jpg" unsized priority />
             </Grid>
             <Grid item xs={12} md={6} lg={4}>
-              <Image src="/images/3.jpg" unsized priority/>
+              <Image src="/images/3.jpg" unsized priority />
             </Grid>
             <Grid item xs={12} md={6} lg={4}>
-              <Image src="/images/4.jpg" unsized priority/>
+              <Image src="/images/4.jpg" unsized priority />
             </Grid>
             <Grid item xs={12} md={6} lg={4}>
-              <Image src="/images/5.jpg" unsized priority/>
+              <Image src="/images/5.png" unsized priority />
             </Grid>
           </Grid>
         </div>

7840c82fb291fceded633eb673243397b826193f
khuzaima <khuzaimayaseen@gmail.com>
2021-01-07 17:43:18 +0500

    Add Image Component in Slider.js

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,41 +1,28 @@
 import React from "react";
+import Image from "next/image";
 
 import { carouselData } from "../../Data/index";
 
 import { Typography } from "@material-ui/core";
 import { Carousel } from "react-bootstrap";
 
+import classes from "./Slider.module.css";
+
 const Slider = () => {
   return (
     <Carousel>
       {carouselData.map((slide, index) => (
         <Carousel.Item interval={2000} key={index}>
-          <div
-            style={{
-              height: "90vh",
-              background: `url('${slide.src}') no-repeat center center/cover`,
-            }}
-          >
-            <div
-              style={{
-                height: "100%",
-                background: "rgba(0, 0, 0, 0.6)",
-                display: "flex",
-                justifyContent: "center",
-                textAlign: "center",
-                alignItems: "center",
-              }}
-            >
+          <div className={classes.warper}>
+            <Image
+              objectFit="cover"
+              layout="fill"
+              loading="eager"
+              src={slide.src}
+            />
+            <div className={classes.overlay}>
               <div style={{ color: "#fff", maxWidth: "75%" }}>
-                <Typography
-                  style={{
-                    lineHeight: 1.8,
-                    wordSpacing: "5px",
-                    fontWeight: "bold",
-                    letterSpacing: "5px",
-                  }}
-                  variant="h4"
-                >
+                <Typography className={classes.content} variant="h4">
                   {slide.txt1}
                 </Typography>
               </div>
